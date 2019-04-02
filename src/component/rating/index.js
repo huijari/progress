a734d098f4584949f4e305c0a327fb8e1287a27b
Alexandre Cesar <acmorcego@gmail.com>
2019-04-02 19:58:20 -0300

    implement rating component

---
+++ src/component/rating/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import useRating from './useRating'
+
+const SubjectNotFound = () => <span>Subject not found.</span>
+const RatingNotFound = () => <span>Rating not found.</span>
+
+const Rating = props => {
+  const {
+    subjectNotFound,
+    ratingNotFound,
+    values,
+    errors = {},
+    isValid,
+    handleChange,
+    handleSubmit
+  } = useRating(props)
+
+  if (subjectNotFound) return <SubjectNotFound />
+  if (ratingNotFound) return <RatingNotFound />
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        name
+        <input
+          name="name"
+          type="text"
+          value={values.name}
+          onChange={handleChange}
+        />
+        <i>{errors.name}</i>
+      </label>
+      <label>
+        total
+        <input
+          name="total"
+          type="number"
+          min={1}
+          value={values.total || ''}
+          onChange={handleChange}
+        />
+        <i>{errors.total}</i>
+      </label>
+      <label>
+        score
+        <input
+          name="score"
+          type="number"
+          min={0}
+          value={values.score || ''}
+          onChange={handleChange}
+        />
+        <i>{errors.score}</i>
+      </label>
+      <button disabled={!isValid}>save</button>
+    </form>
+  )
+}
+
+export default Rating
---
+++ src/component/rating/useRating.js
@@ -0,0 +1,52 @@
+import useForm from '@uneksija/useform'
+
+import RatingService from '../../service/rating'
+import SubjectService from '../../service/subject'
+
+function validate({ name, total }) {
+  const errors = {}
+  if (name === undefined || name === '') errors.name = 'Required'
+  if (total === undefined) errors.total = 'Required'
+  return errors
+}
+
+function useRating({
+  match: {
+    params: { subjectId, ratingId }
+  },
+  history
+}) {
+  const subject = SubjectService.getOne(subjectId)
+  if (subject === undefined) return { subjectNotFound: true }
+
+  const rating = RatingService.getOne(ratingId)
+  if (
+    ratingId !== undefined &&
+    (rating === undefined || !subject.ratings.includes(ratingId))
+  )
+    return { ratingNotFound: true }
+
+  const onSubmit = values => {
+    RatingService.save(values)
+    if (ratingId === undefined) {
+      subject.ratings.push(values.id)
+      SubjectService.save(subject)
+    }
+    history.push(`/s/${subjectId}`)
+  }
+
+  const initialValues = rating || {
+    id: undefined,
+    name: 'Rating',
+    total: 1,
+    score: ''
+  }
+
+  return useForm({
+    initialValues,
+    validate,
+    onSubmit
+  })
+}
+
+export default useRating
