a54fda4f9c61fe5e661fae5a25c52d0125b1378c
Alexandre Cesar <acmorcego@gmail.com>
2019-06-01 17:42:33 -0300

    write basic routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { render } from 'react-dom'
 
-const App = () => <span>hi</span>
+import Rating from './component/rating'
+import SubjectDetail from './component/subjectDetail'
+import SubjectList from './component/subjectList'
+
+const Index = () => (
+  <nav
+    style={{
+      position: 'fixed',
+      right: '8px',
+      border: '1px solid magenta',
+      display: 'flex',
+      flexDirection: 'column',
+      padding: '8px'
+    }}
+  >
+    <b>subject</b>
+    <Link to="/s/nok">subject not found</Link>
+    <Link to="/s/sss">subject</Link>
+    <hr />
+    <b>rating</b>
+    <Link to="/s/nok/r/">subject not found</Link>
+    <Link to="/s/sss/r/nok">rating not found</Link>
+    <Link to="/s/sss/r/">new rating</Link>
+    <Link to="/s/sss/r/XUwVl58mwca6RZrlDqRoS">rating</Link>
+  </nav>
+)
+
+const NotFound = () => '404'
+
+const App = () => (
+  <Router>
+    <Route path="/" component={Index} />
+    <Switch>
+      <Route exact path="/s/:subjectId/r/:ratingId?" component={Rating} />
+      <Route exact path="/s/:subjectId" component={SubjectDetail} />
+      <Route exact path="/" component={SubjectList} />
+      <Route exact component={NotFound} />
+    </Switch>
+  </Router>
+)
 
 render(<App />, document.getElementById('root'))
