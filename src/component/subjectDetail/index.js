c44b76d50353f02e612c66e45e23820643889914
Alexandre Cesar <acmorcego@gmail.com>
2019-06-02 00:01:25 -0300

    style subject detail

--- src/component/subjectDetail/index.js
+++ src/component/subjectDetail/index.js
@@ -1,32 +1,46 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from '@reach/router'
 
-import Editable from '../editable'
-import ScoreChart from '../scoreChart'
 import useSubjectDetail from './useSubjectDetail'
+
+import './style.css'
 
 const SubjectNotFound = () => <span>subject not found</span>
 
+const scoreString = (score, total) => {
+  const value = score ? score.toFixed(0) : '?'
+  return `${value}/${total.toFixed(0).padStart(2, '0')}`
+}
+
 const SubjectDetail = props => {
-  const { subjectNotFound, id, name, changeName, ratings } = useSubjectDetail(
-    props
-  )
+  const { subjectNotFound, id, name, ratings } = useSubjectDetail(props)
 
   if (subjectNotFound) return <SubjectNotFound />
 
   return (
     <div>
-      <ScoreChart ratings={ratings} />
-      <Editable value={name} onChange={changeName} />
-      <ul>
-        {ratings.map(rating => (
-          <li key={rating.id}>
-            {rating.name}
-            <Link to={`/s/${id}/r/${rating.id}`}>edit</Link>
-          </li>
-        ))}
-      </ul>
-      <Link to={`/s/${id}/r/`}>new rating</Link>
+      <div className="subjectDetail__figure" />
+      <div className="subjectDetail__content">
+        <h1 className="subjectDetail__header">{name}</h1>
+        <div className="subjectDetail__grid">
+          {ratings.map(rating => (
+            <Fragment key={rating.id}>
+              <Link
+                className="subjectDetail__rating"
+                to={`/s/${id}/r/${rating.id}`}
+              >
+                {rating.name}
+              </Link>
+              <span className="subjectDetail__ratingScore">
+                {scoreString(rating.score, rating.total)}
+              </span>
+            </Fragment>
+          ))}
+        </div>
+        <Link className="subjectDetail__button" to={`/s/${id}/r/`}>
+          add rating
+        </Link>
+      </div>
     </div>
   )
 }
